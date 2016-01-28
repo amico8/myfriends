b096bc3a5a3ccf6f97a210bfaf47d2387d1a214a
amico <amico8amico@gmail.com>
2016-01-28 13:09:49 +0800

    edit.phpのセレクトボックス部分反映

--- edit.php
+++ edit.php
@@ -102,7 +102,11 @@ $dbh = null;
                 <select class="form-control" name="area_id">
                   <option value="0">出身地を選択</option>
               <?php foreach ($areas as $area) { ?>
+              <?php if ($area['area_id'] == $friends['area_id']) { ?>
                   <option value="<?php echo $area['area_id']; ?>" selected><?php echo $area['area_name']; ?></option>
+              <?php } else{ ?>
+                  <option value="<?php echo $area['area_id']; ?>"><?php echo $area['area_name']; ?></option>
+              <?php } ?>
               <?php } ?>
                 </select>
               </div>
